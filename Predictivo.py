5285967199d9c19b23d2cfe944d7cdc35bd66d6f
Agustin Herbozo <113875509+Agusherbo@users.noreply.github.com>
2024-07-04 18:03:44 -0300

    Update Predictivo.py

--- Predictivo.py
+++ Predictivo.py
@@ -1,14 +1,9 @@
 import streamlit as st
-import pandas as pd 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import mean_squared_error
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import OneHotEncoder
 from xgboost import XGBRegressor
 
 # Función para cargar datos
